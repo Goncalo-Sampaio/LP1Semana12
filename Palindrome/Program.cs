0518baa7f56f12d18c6d5bfb1b568709ac60a950
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-31 10:06:45 +0100

    Add XML and comments

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -3,10 +3,19 @@ using System.Linq;
 
 namespace Palindrome
 {
+    /// <summary>
+    /// Main class for this project
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method of the program which will iterate through the arguments
+        /// to check if they are palindromes
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //Iterates through each arg
             foreach (string arg in args)
             {
                 bool isPalindrome = Palindrome(arg);
@@ -23,6 +32,7 @@ namespace Palindrome
         /// <exception cref="ArgumentNullException"></exception>
         private static bool Palindrome(string input)
         {
+            //Local method that handles the logic to check if its palindrome
             bool IsPalindrome(string input1)
             {
                 return input1.SequenceEqual(input1.Reverse());
@@ -30,14 +40,16 @@ namespace Palindrome
 
             bool result;
 
+            //throws an exception if the string is null
             if (input == null)
             {
                 throw new ArgumentNullException("String can't be null");
             }
 
+            //true if the string length is less than 2
             if (input.Length < 2)
             {
-                result = false;
+                result = true;
             }
             else
             {
